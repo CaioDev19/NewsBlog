99cb626d731a1115b71e151ed4ce0f2748e7d11f
Paulo Vitor <pvitor.dev@ibm.com>
2023-01-13 11:14:22 -0300

    ok

--- server/src/controllers/admin.js
+++ server/src/controllers/admin.js
@@ -1,7 +1,7 @@
 module.exports = {
   async adminLogin(req, res) {
     try {
-      res.send(200).json({ message: 'Testando' });
+      res.send(200).json({ message: 'Testsando' });
     } catch (error) {
       res.send(500).json({ message: 'Erro' });
     }
