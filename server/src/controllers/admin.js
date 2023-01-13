1648f1520371739a7c2e2b1dd0055bf96ad08dfc
Paulo Vitor <pvitor.dev@gmail.com>
2023-01-13 11:39:20 -0300

    porfavor funciona

--- server/src/controllers/admin.js
+++ server/src/controllers/admin.js
@@ -1,7 +1,7 @@
 module.exports = {
   async adminLogin(req, res) {
     try {
-      res.send(200).json({ message: 'Testsando' });
+      res.send(200).json({ message: 'Tsestsando' });
     } catch (error) {
       res.send(500).json({ message: 'Erro' });
     }
