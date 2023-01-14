0732f05686d189231e04ca2527714ba755cfd8f5
Caio Oliveira de Araújo <caiooliveira9.co@gmail.com>
2023-01-14 17:04:28 -0300

    HotFix

--- client/src/components/MainSection/style.ts
+++ client/src/components/MainSection/style.ts
@@ -7,7 +7,7 @@ export const MainContainer = styled(ContentContainer)`
   justify-content: space-between;
   gap: 2.5rem;
 
-  @media (max-width: ${({ theme }) => theme.BREAKPOINTS.tablet}px) {
+  @media (max-width: ${({ theme }) => theme.BREAKPOINTS.notbook}px) {
     flex-direction: column;
     justify-content: center;
     align-items: center;
@@ -17,6 +17,13 @@ export const AddContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 1.25rem;
+
+  @media (max-width: ${({ theme }) => theme.BREAKPOINTS.notbook}px) {
+    border-top: 1px dotted ${({ theme }) => theme.COLORS.black};
+    width: 100%;
+    align-items: center;
+    padding-top: 2.5rem;
+  }
 `
 export const Ad = styled.img`
   width: 480px;
