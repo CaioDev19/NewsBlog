af1d32283b6aa3c4d5bc81312efaa932d5e27140
Caio Oliveira de Araújo <caiooliveira9.co@gmail.com>
2023-03-09 13:05:40 -0300

    format date server

--- server/src/utils/date.js
+++ server/src/utils/date.js
@@ -1,7 +1,7 @@
 const moment = require("moment-timezone")
 
 function formatToBrTimeZone(date) {
-  return moment(date).tz("America/Sao_Paulo").format()
+  return moment(date).tz("America/Sao_Paulo").format("DD/MM/YYYY")
 }
 
 module.exports = {
