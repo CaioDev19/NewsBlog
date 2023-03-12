9bab1475c17ece638a7719a05dc3e456b5d71efe
Caio Oliveira de Araújo <caiooliveira9.co@gmail.com>
2023-03-12 10:30:53 -0300

    First commit branch

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -2,6 +2,7 @@ import { HeroSection } from "../../components/Sections/HeroSection"
 import { MainSection } from "../../components/Sections/MainSection"
 
 export function Home() {
+  console.log("teste")
   return (
     <>
       <HeroSection primary />
