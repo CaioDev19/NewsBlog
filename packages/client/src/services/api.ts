991564e35d82b14d6cfea8c15ff5920a9cebf0d7
Caio Oliveira de Araújo <caiooliveira9.co@gmail.com>
2023-03-14 11:28:53 -0300

    Change banner

--- packages/client/src/services/api.ts
+++ packages/client/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 
 //https://newsblog-production.up.railway.app
 export const api = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://newsblog-production.up.railway.app",
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
